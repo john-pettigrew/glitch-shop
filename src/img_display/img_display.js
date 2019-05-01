e7a6aa1206c7c78d1f23564f3f0e2d24ca28ee7d
John Pettigrew <john.pettigrew404@gmail.com>
2019-04-30 21:32:01 -0500

    clean up image display

--- src/img_display/img_display.js
+++ src/img_display/img_display.js
@@ -11,12 +11,14 @@ class ImgDisplay extends React.Component {
     if(this.props.imgData){
         const canvas = this.canvasRef;
         const ctx = canvas.getContext('2d');
-        ctx.drawImage(this.props.imgData, 0, 0);
+        ctx.drawImage(this.props.imgData, 0, 0, 500, 500);
     }
 
     return (
       <div className="img_display">
-        <canvas 
+        <canvas
+          width="500"
+          height="500"
             ref={ref => {this.canvasRef = ref;}} 
         ></canvas>
       </div>
